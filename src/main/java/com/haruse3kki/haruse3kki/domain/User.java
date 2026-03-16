bd64e595d1d51691a0539d8627b74591ba34fc33
HaDaHyeon <hada3817@gmail.com>
2026-03-16 22:25:33 +0900

    fix: UserDetails 추가

--- src/main/java/com/haruse3kki/haruse3kki/domain/User.java
+++ src/main/java/com/haruse3kki/haruse3kki/domain/User.java
@@ -4,15 +4,19 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,5 +39,21 @@ public class User {
         this.provider = provider;
         this.providerId = providerId;
         this.createdAt = LocalDateTime.now();
+    }
+
+    // UserDetails 구현 메서드들
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return providerId;
     }
 }
